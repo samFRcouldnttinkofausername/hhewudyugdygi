e33995dce88bacbb38194e7d3fb2646ee5948da5
samFRcouldnttinkofausername <122544219+samFRcouldnttinkofausername@users.noreply.github.com>
2023-05-17 09:37:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+joystickbit.initJoystickBit()
+radio.setGroup(31)
+basic.showString("T")
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 10) {
         radio.sendNumber(4)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 1010) {
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 1010) {
         radio.sendNumber(3)
     } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 10) {
         radio.sendNumber(2)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 1010) {
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 1010) {
         radio.sendNumber(1)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
         radio.sendNumber(5)
